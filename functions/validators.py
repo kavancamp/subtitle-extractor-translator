b29145129ce3205fb08d93ef2ca15244c528954e
kavancamp <keenahvancampenhout@gmail.com>
2025-08-16 22:59:31 -0500

    added validate extension

--- functions/validators.py
+++ functions/validators.py
@@ -1,8 +1,8 @@
-# functions/validators.py
-import gettext
 from pathlib import Path
 
 import click
+
+from functions.i18n import _
 
 VALID_VIDEO_EXTENSIONS = {
     ".mp4",
@@ -15,13 +15,34 @@ VALID_VIDEO_EXTENSIONS = {
 
 
 def validate_video_extension(ctx, param, value):
-    _ = gettext.gettext
     ext = Path(value).suffix.lower()
     if ext not in VALID_VIDEO_EXTENSIONS:
+        kinds = ", ".join(sorted(VALID_VIDEO_EXTENSIONS))
         raise click.BadParameter(
-            _(
-                f"Invalid video format: '{ext}'. Must be one of: "
-                "{', '.join(sorted(VALID_VIDEO_EXTENSIONS))}"
-            )
+            _("Invalid video format: '")
+            + ext
+            + _("'. Must be one of: ")
+            + kinds
         )
     return value
+
+
+def validate_extension(
+    ctx, param, value, allowed_exts=(".srt", ".txt")
+):
+    if value:
+        ext = Path(value).suffix.lower()
+        if ext not in allowed_exts:
+            allowed = ", ".join(allowed_exts)
+            raise click.BadParameter(
+                _("File must end with: ") + allowed
+            )
+    return value
+
+
+def validate_srt(ctx, param, value):
+    return validate_extension(ctx, param, value, (".srt",))
+
+
+def validate_txt(ctx, param, value):
+    return validate_extension(ctx, param, value, (".txt",))
